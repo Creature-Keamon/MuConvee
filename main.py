d08025cf2a49c1c714686bd3fa8191c06264232f
Creature-Keamon <Shuttlez4@outlook.com>
2025-07-16 15:49:45 +1200

    experimenting with get requests

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 import base64
 
-client_id = "5ecb385927d94694819928aa033f888c"
-client_secret = "faaf18e18019452b8a116b64bab4aaff"
+client_id = "Client ID"
+client_secret = "Client Secret"
 url = "https://accounts.spotify.com/api/token"
-headers = {"Authorisation": "Basic " + base64.b64encode(f"{client_id}:{client_secret}".encode('utf-8')).decode('utf-8')}
+headers = {"Authorization": "Basic " + base64.b64encode(f"{client_id}:{client_secret}".encode('utf-8')).decode('utf-8')}
 form = {'grant_type': 'client_credentials'}
 
 response = requests.post(url,form, headers= headers)
@@ -12,5 +12,10 @@ response = requests.post(url,form, headers= headers)
 if response.status_code == 200:
     token_data = response.json()
     print(token_data)
-else:
-    print(f"Error: {response.status_code}")
+    url = "https://open.spotify.com/artist/6fuALtryzj4cq7vkglKLxq"
+    print(token_data['access_token'])
+    headers = {"Authorization": "Bearer " + base64.b64encode(token_data['access_token'].encode('utf-8')).decode('utf-8')}
+    response2 = requests.get(url, form, headers=headers)
+    print(response2)
+    data = response2.json()
+    print(data)
