74518ba955c62c4d5658b02f1637fa6a92c92ff8
Creature-Keamon <Shuttlez4@outlook.com>
2025-07-16 21:43:49 +1200

    learning how to fetch data

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 import base64
 
-client_id = "Client ID"
-client_secret = "Client Secret"
+client_id = ""
+client_secret = ""
 url = "https://accounts.spotify.com/api/token"
 headers = {"Authorization": "Basic " + base64.b64encode(f"{client_id}:{client_secret}".encode('utf-8')).decode('utf-8')}
 form = {'grant_type': 'client_credentials'}
@@ -12,10 +12,11 @@ response = requests.post(url,form, headers= headers)
 if response.status_code == 200:
     token_data = response.json()
     print(token_data)
-    url = "https://open.spotify.com/artist/6fuALtryzj4cq7vkglKLxq"
+    url = "https://api.spotify.com/v1/artists/6fuALtryzj4cq7vkglKLxq"
     print(token_data['access_token'])
-    headers = {"Authorization": "Bearer " + base64.b64encode(token_data['access_token'].encode('utf-8')).decode('utf-8')}
+    headers = {"Authorization": "Bearer " + token_data['access_token']}
     response2 = requests.get(url, form, headers=headers)
-    print(response2)
     data = response2.json()
-    print(data)
+    for key, value in data.items():
+        print(key,value)
+    
