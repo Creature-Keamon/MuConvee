543f53b500d87834312e5ff4a868896ad828618b
Creature-Keamon <Shuttlez4@outlook.com>
2025-07-17 21:16:36 +1200

    search works, now to fetch correct data

--- api_call_helper.py
+++ api_call_helper.py
@@ -1,8 +1,8 @@
 import requests
 import base64
 
-spotify_client_id = ""
-spotify_client_secret = ""
+spotify_client_id = "5ecb385927d94694819928aa033f888c"
+spotify_client_secret = "7b5b6a9f89ea45fb9bd55773c6c838ba"
 
 def establish_spotify_connection(client_id, client_secret):
     """Given a client ID, client secret and API URL, a connection request to Spotify
@@ -20,34 +20,42 @@ def establish_spotify_connection(client_id, client_secret):
     else:
         raise ConnectionRefusedError(f"data not received, received code {response.status_code} instead.")
     
-def spotify_search(search_query, access_token, type=0):
+def spotify_search_track(search_query, access_token, type=0):
     
     query_split = list(search_query.split(" "))
     url = "https://api.spotify.com/v1/search?q="
-    if query_split[0][:-1].lower() in {"track", "album", "artist", "year", "genre"}:
-        url += query_split[0][:-1]
+
     prev_token = None
+    
     for item in query_split:
-        if item[:-1].lower() in {"track", "album", "artist", "year", "genre"}:
-            url += "%2520"+ item[:-1]
+        if item.lower() in {"track:", "album:", "artist:", "year:", "genre:"}:
+            url += "%2520"+ item[:-1] + "%3A"
             prev_token = "key"
+        
         else:
-            if prev_token == "value":
-                url += "%2520" + item
+            if ":" in item:
+                print(item)
+                item = item.replace(":", "%3A")
+                url += item
+                prev_token = "value + semi"
+
             else:
-                url += "%3A" + item
-            prev_token = "value"
+                if prev_token in {"key","value + semi"}:
+                    url += item
+                else:
+                    url += "%2520" + item
+                prev_token = "value"
+    url += "&type=track"
     headers = {"Authorization": "Bearer " + access_token}
-    print(url)
     data = requests.get(url,headers=headers)
-    print(data.status_code)
-
-
+    data = data.json()
+    print(url)
+    print(data)
 
 
         
 
 token_data = establish_spotify_connection(spotify_client_id, spotify_client_secret)
     
-spotify_search("track: Devonian: Nascent artist: The Ocean", token_data["access_token"])
+spotify_search_track("track: Subboreal artist: The Ocean", token_data["access_token"])
     
